f68aa7e96e7deca5e9d697a54b3a2b2352d5cfb7
Juan Lizcano <juanliizp12@gmail.com>
2017-10-12 18:40:37 -0500

    suma y **

---
+++ multiplicarvector.py
@@ -0,0 +1,22 @@
+import random
+
+
+n = int(input("Ingrese n: "))
+
+arr__uno = [None] * n
+arr__dos = [None] * n
+suma__arr = [None] * n
+
+
+for i in range(0, n):
+  arr__uno[i] = random.randint(1, 100)
+  arr__dos[i] = random.randint(1, 100)
+
+print "arrUno = ", arr__uno
+print "arrDos = ", arr__dos
+
+for i in range(0, n):
+  suma__arr[i] = arr__uno[i] * arr__dos[i]
+
+print "Suma de Vectores"
+print suma__arr
