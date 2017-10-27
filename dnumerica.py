2001c0422eadf0b7855848a0b358299bbec5d59c
Juan Lizcano <juanliizp12@gmail.com>
2017-10-26 21:43:26 -0500

    alumnos

--- dnumerica.py
+++ dnumerica.py
@@ -1,13 +1,16 @@
 import math
+#importa las funciones matematicas
 
 def f(x):
   return 1/x
-
+#funcion f
 
 def fx(x):
   return -1/ math.pow(x,2) 
+#funcion fx
 
 
+#Declaracion de variables
 punto_inicial = 0.0
 interaciones = 0.0
 euler = 0.0
@@ -17,18 +20,22 @@ i = 0.0
 aproximado = 0.0
 exacta = 0.0
 
+
+#Ingresa valores 
 punto_inicial = float(raw_input("Ingrese Numero Inicial: "))
 euler = float(raw_input("Ingrese Euler: "))
 distancia = float(raw_input("Ingrese Distancia: "))
 interaciones = int(raw_input("Ingrese Interacciones: "))
 
 
+#si la distancia es 0 se muere :D
 if(distancia == 0 ):
     print "Error de indeterminacion"
 
-
+#Calculamos el error
 err = abs( f(punto_inicial) - fx(punto_inicial))
 
+#calcular la difenciacion 
 while (distancia != 0  and (err < euler) and (i <= interaciones)):
   aproximado = (f(punto_inicial + distancia) - f(punto_inicial) / distancia)
   exacta  = fx(punto_inicial)
@@ -36,11 +43,23 @@ while (distancia != 0  and (err < euler) and (i <= interaciones)):
   i = i + 1
   distancia = distancia / 2 
 
+
 if( err < euler):
   print "diferenciacion aproximada: " , aproximado
 
-if(i <= interaciones):
+
+if(i < interaciones):
   print "diferenciacion aproximada: " , aproximado
 
 
+"""
+ALUMNOS
+AUTOR : JUAN JOSE LIZCANO
 
+RAFAEL MANJARES
+CARLOS DIAZ
+STEFANY PALOMAR
+PEDRO PABLO PAQUE
+JORGE ENRRIQUE RAMIREZ
+
+"""
